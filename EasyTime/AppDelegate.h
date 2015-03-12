9d22de21d9d85ac83208416da0c5853955de7e32
saurabhvmobi <saurabhs@vmpc1147.vmoksha.com>
2015-03-12 18:33:20 +0530

    Initial Commit

---
+++ EasyTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyTime
+//
+//  Created by Saurabh Suman on 12/03/15.
+//  Copyright (c) 2015 Vmoksha Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EasyTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EasyTime
+//
+//  Created by Saurabh Suman on 12/03/15.
+//  Copyright (c) 2015 Vmoksha Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
